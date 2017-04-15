617c8ea71cbb1d82b87c0b3a4fe00b1b5cd633c8
Alex LaFroscia <alex@lafroscia.com>
2017-04-15 13:43:03 -0700

    Remove unused methods from the file controller

--- apps/web_server/web/controllers/file_controller.ex
+++ apps/web_server/web/controllers/file_controller.ex
@@ -22,25 +22,4 @@ defmodule Serv.WebServer.FileController do
         |> render(Serv.WebServer.ErrorView, "404.json", %{})
     end
   end
-
-  def file_content(conn, %{"file_name" => file_name}) do
-    case Serv.FileManager.get_file(file_name) do
-      {:ok, file} ->
-        conn
-        |> render_content(file)
-      {:error, :not_found} ->
-        conn
-        |> put_status(:not_found)
-        |> text("")
-    end
-  end
-
-  defp render_content(conn, file) do
-    content = file
-              |> Serv.File.get_instances
-              |> Enum.at(0)
-              |> Serv.FileInstance.get_content
-
-    text(conn, content)
-  end
 end
