48eba62548c9922bd2216a0744d70253bfbafa01
Alex LaFroscia <alex@lafroscia.com>
2017-09-01 15:26:24 -0700

    Add linting and style formatting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  parser: 'babel-eslint',
+  env: { browser: true },
+  extends: ['eslint:recommended', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'react'],
+  rules: {
+    // Prettier
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true
+      }
+    ],
+
+    // JSX
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error'
+  },
+  settings: {
+    react: {
+      pragma: 'h'
+    }
+  }
+};
--- ui/pages/files.js
+++ ui/pages/files.js
@@ -2,8 +2,10 @@ import { h, Component } from 'preact';
 
 export default class extends Component {
   render() {
-    return (<div>
-      <h1>Files</h1>
-    </div>);
+    return (
+      <div>
+        <h1>Files</h1>
+      </div>
+    );
   }
 }
