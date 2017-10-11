f22b2c804b9814a0929db1b41f15e4cae1bd73f6
Alex LaFroscia <alex@lafroscia.com>
2017-10-11 13:08:49 -0700

    Update dev config for peerage

--- apps/file_server/config/dev.exs
+++ apps/file_server/config/dev.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
-config :peerage, via: Peerage.Via.Dns,
-  dns_name: "localhost",
-  app_name: "api-server"
+config :peerage, via: Peerage.Via.List,
+  node_list: [
+    :"api-server@127.0.0.1"
+  ]
